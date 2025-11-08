e82a49fb4af2f7564e08085d0bff4daa2903cdbd
kweku-annan <kwekuannan.dev@gmail.com>
2025-11-08 20:17:11 +0000

    Add corrections to perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -19,13 +19,14 @@ SELECT
     pay.payment_method,
     pay.status
 FROM 
-    bookings b
-JOIN 
-    users u ON b.user_id = u.user_id
-JOIN 
-    properties p ON b.property_id = p.property_id
-JOIN 
-    payments pay ON b.booking_id = pay.booking_id
+    bookings b,
+    users u,
+    properties p,
+    payments pay
+WHERE 
+    b.user_id = u.user_id
+    AND b.property_id = p.property_id
+    AND b.booking_id = pay.booking_id
 ORDER BY 
     b.booking_date DESC;
 
